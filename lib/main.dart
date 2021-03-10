0d917efebde3061bbcb602f6589191c4a42bd1af
João Soares <joao.f.soares@gmail.com>
2021-03-10 14:17:07 -0100

    Improved readability across platforms. Improved code structure.

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_platform_experiment/production_server_address.dart';
 import 'package:universal_platform/universal_platform.dart';
 import 'package:flutter_platform_experiment/socket_channel_manager.dart';
 
+// Production flag
 const bool PRODUCTION = false;
-
-String webSocketServer(){
-  if(PRODUCTION){
-    return PROD_SERVER;
-  }else {
-    if (UniversalPlatform.isAndroid) {
-      return 'ws://10.0.2.2:3000';
-    } else {
-      return 'ws://127.0.0.1:3000';
-    }
-  }
-}
 
 void main() => runApp(
   MaterialApp(home: MyApp()),
@@ -28,12 +18,8 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   var channel;
-
-  // final HtmlWebSocketChannel
   TextEditingController _textEditingController = TextEditingController();
   List<String> messages = [];
-
-
 
   @override
   void initState() {
@@ -61,36 +47,10 @@ class _MyAppState extends State<MyApp> {
         padding: const EdgeInsets.all(8.0),
         child: Column(
           children: <Widget>[
-            Expanded(
-              child: ListView.builder(
-                reverse: true,
-                itemCount: messages.length,
-                itemBuilder: (context, index) {
-                  return Text(messages[index] == null
-                      ? ""
-                      : '${messages[index]}',
-                  style: TextStyle(
-                    fontSize: 30,
-                  ));
-                }
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Expanded(
-                  child: TextFormField(
-                    controller: _textEditingController,
-                    onFieldSubmitted: (_) => sendMessage(),
-                    autofocus: true,
-                  ),
-                ),
-                TextButton.icon(
-                  onPressed: sendMessage,
-                  icon: Icon(Icons.send),
-                  label: Text("Send")
-                ),
-              ],
+            MessageList(messages),
+            Controls(
+              textEditingController: _textEditingController,
+              sendMessage: sendMessage,
             ),
           ],
         ),
@@ -98,16 +58,32 @@ class _MyAppState extends State<MyApp> {
     );
   }
 
-  void sendToWebSocket(String message) {
-    String platform = getPlatform();
-    channel.sink.add('$platform: $message');
-  }
-
+  // Actions
   void sendMessage(){
     sendToWebSocket(_textEditingController.text);
     _textEditingController.clear();
   }
 
+  // Operations
+  void sendToWebSocket(String message) {
+    String platform = getPlatform();
+    channel.sink.add('$platform: $message');
+  }
+
+  // Address helper method
+  String webSocketServer(){
+    if(PRODUCTION){
+      return PROD_SERVER;
+    }else {
+      if (UniversalPlatform.isAndroid) {
+        return 'ws://10.0.2.2:3000';
+      } else {
+        return 'ws://127.0.0.1:3000';
+      }
+    }
+  }
+
+  // Platform String helper
   String getPlatform(){
     if(UniversalPlatform.isWeb)
       return 'Web';
@@ -131,5 +107,109 @@ class _MyAppState extends State<MyApp> {
       return 'Windows';
 
     return 'Platform?';
+  }
+}
+
+// UI Widgets
+class MessageList extends StatelessWidget {
+  final List<String> messages;
+
+  MessageList(this.messages);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: ListView.builder(
+        reverse: true,
+        itemCount: messages.length,
+        itemBuilder: (context, index) {
+          return Message(messages[index]);
+        }
+      ),
+    );
+  }
+}
+
+class Controls extends StatelessWidget {
+  final TextEditingController textEditingController;
+  final Function sendMessage;
+
+  Controls({
+    @required this.textEditingController,
+    @required this.sendMessage
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Expanded(
+          child: TextFormField(
+            controller: textEditingController,
+            onFieldSubmitted: (_) => sendMessage(),
+            autofocus: true,
+          ),
+        ),
+        TextButton.icon(
+            onPressed: sendMessage,
+            icon: Icon(Icons.send),
+            label: Text("Send")
+        ),
+      ],
+    );
+  }
+}
+
+class Message extends StatelessWidget {
+  final String message;
+
+  Message(this.message);
+
+  @override
+  Widget build(BuildContext context) {
+    print(message);
+    List<String> _complexMessage = message.split(':');
+    print(_complexMessage);
+    return TextBubble(
+      platform: _complexMessage[0],
+      message: _complexMessage[1].trimRight().toString(),
+    );
+  }
+}
+
+class TextBubble extends StatelessWidget {
+  final String platform;
+  final String message;
+
+  TextBubble({
+    @required this.message,
+    @required this.platform
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return RichText(
+      text: TextSpan(
+        children: <TextSpan>[
+          TextSpan(
+            text: platform+': ',
+            style: TextStyle(
+              color: Colors.blueGrey,
+              fontWeight: FontWeight.w600,
+              fontSize: 30,
+            ),
+          ),
+          TextSpan(
+            text: message,
+            style: TextStyle(
+              color: Colors.black,
+              fontStyle: FontStyle.italic,
+              fontSize: 30,
+            ),
+          ),
+        ]
+      ),
+    );
   }
 }
